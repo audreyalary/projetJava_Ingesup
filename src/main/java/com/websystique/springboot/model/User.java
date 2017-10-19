98f71d48f8dd84bdef18cee7d3228ff5bde05300
florian <florian@MacBook-Pro-de-florian.local>
2017-10-19 11:01:52 +0200

    Push initial Spring

---
+++ src/main/java/com/websystique/springboot/model/User.java
@@ -0,0 +1,85 @@
+package com.websystique.springboot.model;
+	
+public class User {
+
+	private long id;
+	
+	private String name;
+	
+	private String LastIP;
+	
+	private double CPUCharge;
+
+	public User(){
+		id=0;
+	}
+	
+	public User(long id, String name, String LastIP, double CPUCharge){
+		this.id = id;
+		this.name = name;
+		this.LastIP = LastIP;
+		this.CPUCharge = CPUCharge;
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastIP() {
+		return LastIP;
+	}
+
+	public void setLastIP(String LastIP) {
+		this.LastIP = LastIP;
+	}
+
+	public double getCPUCharge() {
+		return CPUCharge;
+	}
+
+	public void setCPUCharge(double CPUCharge) {
+		this.CPUCharge = CPUCharge;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", LastIP=" + LastIP
+				+ ", CPUCharge=" + CPUCharge + "]";
+	}
+
+
+}
